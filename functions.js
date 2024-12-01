f53af5f628184142738ad329d6c5786d5f9e1055
Tadas Kiaunė <sorbinas30@gmail.com>
2024-12-01 14:24:44 +0200

    10 laboras update in progress

--- functions.js
+++ functions.js
@@ -1,6 +1,15 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const toggleButton = document.querySelector('.light-dark-button');
-    toggleButton.addEventListener('click', () => {
-        document.body.classList.toggle('dark-mode');
-    });
+const icon = document.getElementById('l-d-b-icon');
+const sidebar = document.getElementById('sidebar');
+
+let isDarkMode = false;
+
+document.getElementById('darkModeToggle').addEventListener('click', () => {
+    isDarkMode = !isDarkMode;
+    icon.textContent = isDarkMode ? 'dark_mode' : 'light_mode';
+
+    document.body.classList.toggle('dark-mode');
+});
+
+document.getElementById('navEnableBut').addEventListener('click', () => {
+    document.body.classList.toggle('hidden');
 });
