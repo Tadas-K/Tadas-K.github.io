d4e916eb1ae1c94c653c5d6ed51e0fdaea75df60
Tadas Kiaunė <sorbinas30@gmail.com>
2024-12-02 10:06:09 +0200

    Started 12 laboras

--- functions.js
+++ functions.js
@@ -44,3 +44,41 @@ setInterval(() => {
     timeParagraph.textContent = timeString;
 
 }, 1000);
+
+
+
+// Forma
+class FormObject {
+    constructor(name, surename, email, phoneNumb, address){
+        this.name = name;
+        this.surename = surename;
+        this.email = email;
+        this.phoneNumb = phoneNumb;
+        this.address = address;
+    }
+
+    getFormatedOutput(){
+        return `${this.name}, ${this.surename}, ${this.email}, ${this.phoneNumb}, ${this.address}`;
+    }
+}
+
+
+// function(event) naudojama vietoj () => {}, nes norime tureti ref i pradini 
+// formos objekta, kad galetume paimti reiksmes, jeigu to nereikia, tai galima
+// naudoti tuscia varianta () => {}
+document.getElementById('form-id').addEventListener('submit', function(event) {
+    // Neleidzia submitinti formos
+    event.preventDefault();
+
+    const name = document.getElementById("name-id").value;
+    const surename = document.getElementById("surname-id").value;
+    const email = document.getElementById("email-id").value;
+    const phone = document.getElementById("phone-id").value;
+    const address = document.getElementById("adress-id").value;
+
+    let formAnswer = new FormObject(name, surename, email, phone, address);
+
+    document.getElementById('form-output-id').value = formAnswer.getFormatedOutput();
+
+    console.log(formAnswer);
+})
