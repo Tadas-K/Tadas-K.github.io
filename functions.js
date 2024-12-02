ec88102cc5c954e67118a3f3056d6ba45ebcec84
Tadas Kiaunė <sorbinas30@gmail.com>
2024-12-02 14:00:19 +0200

    More 12 laboras progress

--- functions.js
+++ functions.js
@@ -49,16 +49,17 @@ setInterval(() => {
 
 // Forma
 class FormObject {
-    constructor(name, surename, email, phoneNumb, address){
+    constructor(name, surename, email, phoneNumb, address, numbs){
         this.name = name;
         this.surename = surename;
         this.email = email;
         this.phoneNumb = phoneNumb;
         this.address = address;
+        this.numbs = numbs;
     }
 
     getFormatedOutput(){
-        return `${this.name}, ${this.surename}, ${this.email}, ${this.phoneNumb}, ${this.address}`;
+        return `Vardas: ${this.name}\nPavardė: ${this.surename}\nEl. paštas: ${this.email}\nTelefonas: ${this.phoneNumb}\nAdresas: ${this.address}\nĮvesti skaičiai: ${this.numbs.join(", ")}\nViduris: `;
     }
 }
 
@@ -74,11 +75,21 @@ document.getElementById('form-id').addEventListener('submit', function(event) {
     const surename = document.getElementById("surname-id").value;
     const email = document.getElementById("email-id").value;
     const phone = document.getElementById("phone-id").value;
-    const address = document.getElementById("adress-id").value;
+    const address = document.getElementById("address-id").value;
 
-    let formAnswer = new FormObject(name, surename, email, phone, address);
 
-    document.getElementById('form-output-id').value = formAnswer.getFormatedOutput();
+    const numbs = [];
+    const numName = "number-id";
+    for(let i = 1; i <= 5; i++){
+        numbs[i-1] = document.getElementById(`${numName}${i}`).value;
+        
+    }
 
-    console.log(formAnswer);
-})
+    let formAnswer = new FormObject(name, surename, email, phone, address, numbs);
+
+    document.getElementById('form-output-id').innerText = formAnswer.getFormatedOutput();
+
+    console.log(formAnswer.getFormatedOutput());
+});
+
+
